ddf3033759a7f0eb1b12390a119588496e1ca563
Pedro Henrique Santana dos Santos <1801975@SALAS.AULAS>
2019-04-11 21:14:29 -0300

    arquivo inicial da ac

--- lp2/arquivos-aula/banco.py
+++ lp2/arquivos-aula/banco.py
@@ -1,41 +1,55 @@
-__alunos__ = ["pedro.santana@aluno.impacta.com.br",
-              "barbara.santos@aluno.impacta.com.br"]
+from typing import Union, List, Dict
+
+Number = Union[int, float]
+
+__alunos__ = ["pedro.santana@aluno.faculdadeimpacta.com.br",
+              "barbara.guimaroes@aluno.faculdadeimpacta.com.br"]
 
 
 class Cliente():
     """
-    A classe Cliente deve ter os atributos: nome, telefone e email, que
-    serão passados em sua construção.
-    Não é necessário implementar nenhum método para esta classe.
+    Classe Cliente do Banco
     """
-    def __init__(self, nome_cliente, telefone_cliente, email_cliente):
-        self._nome = self.set_nome(nome_cliente)
-        self._email = self.set_email(email_cliente)
-        self._telefone = self.set_telefone(telefone_cliente)
+    def __init__(self, nome: str, telefone: int, email: str):
+        self._nome = nome
+        self.set_telefone(telefone)
+        self.set_email(email)
 
-    def get_nome(self):
+    def get_nome(self) -> str:
+        '''
+        Acessor do atributo Nome
+        '''
         return self._nome
 
-    def set_nome(self, n):
-        self._nome = n
+    def get_telefone(self) -> int:
+        '''
+        Acessor do atributo Telefone
+        '''
+        return self._tel
 
-    def get_email(self):
+    def set_telefone(self, novo_telefone: int) -> None:
+        '''
+        Mutador do atributo telefone, caso não recebe um número,
+        retorna um TypeError
+        '''
+        if not type(novo_telefone) == int:
+            raise TypeError('Telefone deve ser um número')
+        self._tel = novo_telefone
+
+    def get_email(self) -> str:
+        '''
+        Acessor do atributo Email
+        '''
         return self._email
 
-    def set_email(self, e):
-        if "@" in e:
-            self._email = e
-        else:
-            print("E-mail invalido ! :(")
-
-    def get_telefone(self):
-        return self._telefone
-
-    def set_telefone(self, numero):
-        if type(numero) == int:
-            self._telefone = numero
-        else:
-            print("Telefone invalido ! :(")
+    def set_email(self, novo_email: str) -> None:
+        '''
+        Mutador do atributo Email, caso não receba um email válido
+        retorna um ValueError
+        '''
+        if '@' not in novo_email:
+            raise ValueError('Não é um email válido')
+        self._email = novo_email
 
 
 class Banco():
@@ -48,81 +62,154 @@ class Banco():
     DICA2: utilze a variável acima para gerar automaticamente o número das
     contas do banco
     """
-    def __init__(self, nome_banco):
-        self._nome = nome_banco
-        self._lista_contas = []
+    def __init__(self, nome: str) -> None:
+        self._nome = nome
+        self._contas = []
 
-    def get_nome(self):
+    def get_nome(self) -> str:
+        '''
+        Acessor do Atributo Nome
+        '''
         return self._nome
 
-    def set_nome(self, nome_banco):
-        self._nome[0] = self._nome[0].upper()
+    def abre_conta_corrente(self, clientes: List[Cliente],
+                            saldo_inicial: Number,
+                            taxa_manutencao: Number) -> None:
+        '''
+        Método para abertura de nova conta, recebe os clientes
+        o saldo inicial e a taxa de manutenção, caso o saldo inicial
+        seja menor que 0 devolve um ValueError
+        '''
+        if saldo_inicial < 0:
+            raise ValueError('O saldo inicial não pode ser negativo')
+        c = ContaCorrente(clientes, len(self._contas) + 1,
+                          saldo_inicial, taxa_manutencao)
+        self._contas.append(c)
 
-    def get_lista_contas(self):
-        return self._lista_contas
+    def abre_conta_poupanca(self, clientes: List[Cliente],
+                            saldo_inicial: Number, juros: Number) -> None:
+        '''
+        Método para abertura de nova conta, recebe os clientes
+        o saldo inicial e a taxa de manutenção, caso o saldo inicial
+        seja menor que 0 devolve um ValueError
+        '''
+        if saldo_inicial < 0:
+            raise ValueError('O saldo inicial não pode ser negativo')
+        c = ContaPoupanca(clientes, len(self._contas) + 1,
+                          saldo_inicial, juros)
+        self._contas.append(c)
 
-    def set_lista_contas(self):
-        pass
-
-    def abre_contas(self, clientes, s_inicial):
-        conta_nova = Conta(clientes, len(self.lista_contas) + 1, s_inicial)
-        self.lista_contas.append(conta_nova)
-
-    def lista_contas(self):
-        self.get_lista_contas()
+    def lista_contas(self) -> List['Conta']:
+        '''
+        Retorna a lista com todas as contas do banco
+        '''
+        return self._contas
 
 
 class Conta():
     """
-    A Classe conta deverá receber em sua construção os atributos:
-    clientes (uma lista de Clientes), número da conta e um saldo inicial.
-    Ela deverá implementar os métodos:
-    saque(valor): diminui o saldo em valor
-    deposito(valor): aumenta o saldo em valor
-    extrato: exibe todas as transações da conta e o saldo após cada transação
-    resumo():  imprime um resumo da conta (nome dos clientes, numero da conta
-    e saldo)
-    DICA: Para fazer a função extrato faça uma variável interna que que
-    armazene todas as operações de saque e deposito e seus valores.
+    Classe Conta base para outras contas.
     """
-    def __init__(self, lista_clientes, n_conta, s_inicial):
-        self._clientes = lista_clientes
-        self._numero = n_conta
-        self._saldo = s_inicial
-        self._transacoes = []
-        self._transacoes.append({'inicial': s_inicial})
-
-    def get_clientes(self):
-        return self._clientes
-
-    def set_clientes(self):
+    def __init__(self, clientes: List[Cliente], numero_conta: int,
+                 saldo_inicial: Number):
         pass
 
-    def get_numero(self):
-        return self._numero
-
-    def set_numero(self):
+    def get_clientes(self) -> List[Cliente]:
+        '''
+        Acessor para o atributo Clientes
+        '''
         pass
 
-    def get_saldo(self):
-        return self._saldo
-
-    def set_saldo(self):
+    def get_saldo(self) -> Number:
+        '''
+        Acessor para o Atributo Saldo
+        '''
         pass
 
-    def get_transacoes(self):
-        return self._transacoes
-
-    def set_transacoes(self):
+    def set_saldo(self, novo_saldo: Number) -> None:
+        '''
+        Mutador para o atributo Saldo
+        '''
         pass
 
-    def saque(self, valor):
-        self._saldo = self._saldo - valor
-        self._transacoes.append({'saque': valor})
+    def get_numero(self) -> int:
+        '''
+        Acessor para o atributo Numero
+        '''
+        pass
 
-    def deposito(self, valor):
-        self._saldo = self._saldo + valor
-        self._transacoes.append({'deposito': valor})
+    def saque(self, valor: Number) -> None:
+        '''
+        Método de saque da classe Conta, operação deve aparecer no extrato
+        '''
+        pass
 
-    def extrato(self):
-        return self.get_transacoes()
+    def deposito(self, valor: Number):
+        '''
+        Método depósito da classe Conta, operação deve aparecer no extrato
+        '''
+        pass
+
+    def inclui_operacao_extrato(self, operacao: Dict[str, Number]):
+        '''
+        Inclui operação na lista de operações da conta.
+        Uma operação é um dicionário onde a chave éum string com o nome
+        da operação, e o valor e o valor da operação.
+        ex: {'saque':100}, {'deposito': 250.10}
+        '''
+        pass
+
+    def extrato(self) -> List[Dict[str, Number]]:
+        '''
+        Retorna uma lista com as operações executadas na Conta
+        '''
+        pass
+
+
+class ContaPoupanca(Conta):
+    '''
+    Classe Conta Poupança
+    '''
+    def __init__(self, clientes: List[Cliente], numero: int,
+                 saldo_inicial: Number, juros: Number) -> None:
+        pass
+
+    def rendimentos(self) -> None:
+        '''
+        Calcula e adiciona no saldo da conta o valor dos rendimentos
+        com base no saldo e na taxa de juros. esta operação de aparecer
+        no extrato: {'rendimentos': valor}
+        '''
+        pass
+
+    def saque(self, valor: Number) -> None:
+        '''
+        Saca o valor da conta, caso o valor a ser sacado seja
+        maior que o saldo da conta, devolve um ValueError
+        Esta operação deve aparecer no extrato.
+        '''
+        pass
+
+
+class ContaCorrente(Conta):
+    '''
+    Classe Conta Corrente
+    '''
+    def __init__(self, clientes: List[Cliente], numero: int,
+                 saldo_inicial: Number, juros: Number, limite: Number) -> None:
+        pass
+
+    def cobra_juros(self) -> None:
+        '''
+        Cobra a taxa de juros da conta caso esteja dentro do cheque especial,
+        esta operacao deve aparecer no extrato: {'juros': valor}
+        '''
+        pass
+
+    def saque(self, valor: Number) -> None:
+        '''
+        Saca o valor da conta corrente, caso esteja dentro do saldo e do limite
+        do cheque especial, caso contrário devolve um ValueError
+        esta operação deve entrar no extrato
+        '''
+        pass
