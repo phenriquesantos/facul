90450680be95a46ebff14825aead737a2d0c93da
Pedro Henrique <pedro.progsantos@gmail.com>
2019-04-16 22:22:18 -0300

    ac 05 django finalizada

--- tec-web/ac-05/ac05/core/urls.py
+++ tec-web/ac-05/ac05/core/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index')
+    ,path('pagina-2/', views.pagina_2, name="Pagina 2")
+    ,path('pagina-3/', views.pagina_3, name="pagina 3")
 ]
